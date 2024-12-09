c12112e05fa3d8fdfb730bd1fce6e612def919e6
Furkan Demirtaş <furkan-demirtas@outlook.com>
2024-12-09 22:32:42 +0300

    DOM ile HTML sayfasına linkler eklendi ve açılır menü işlevi eklendi

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,40 @@
+const contents = document.querySelector(".contents");
+
+function childElemets(param) {
+  return `
+    <ul class="menuLinks">
+      <li class="menuLink"><span onclick="clickLink(this)">${param.label}</span>
+        ${param.children?.length > 0 ? param.children.map((x) => childElemets(x)).join("") : ""}
+      </li>
+    </ul>
+  `;
+}
+
 async function init() {
   const response = await fetch("/assets/json/data.json").then((x) => x.json());
+  contents.innerHTML = response
+    .map((x) => {
+      return `
+      <ul class="menuLinks">
+        <li class="menuLink"><span onclick="clickLink(this)">${x.label}</span>
+        ${x.children?.length > 0 ? x.children.map((y) => childElemets(y)).join("") : ""}
+        </li>
+      </ul>
+      <hr>
+    `;
+    })
+    .join("");
+}
+
+function clickLink(p) {
+  const treeElements = p.parentElement.parentElement.querySelectorAll(".menuLinks");
+  treeElements.forEach((treeElement) => {
+    if (treeElement != null) {
+      treeElement.querySelectorAll("*").forEach((element) => {
+        element.classList.toggle("hidden");
+      });
+    }
+  });
 }
 
 init();
