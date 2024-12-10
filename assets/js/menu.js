feac54d2c39ca40c5bcabbf7e011ed50942fb019
Furkan Demirtaş <furkan-demirtas@outlook.com>
2024-12-10 18:12:47 +0300

    iyileştirmeler yapıldı

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,40 +1,20 @@
-const contents = document.querySelector(".contents");
+const menuLinks = document.querySelector(".menuLinks");
 
-function childElemets(param) {
+function createItems(param) {
   return `
-    <ul class="menuLinks">
-      <li class="menuLink"><span onclick="clickLink(this)">${param.label}</span>
-        ${param.children?.length > 0 ? param.children.map((x) => childElemets(x)).join("") : ""}
-      </li>
-    </ul>
+    <li class="menuLink">
+      <label>
+        <input type="checkbox">
+        ${param.label}
+      </label>
+      ${param.children?.length > 0 ? '<ul class="menuLinks">' + param.children.map((x) => createItems(x)).join("") + "</ul>" : ""}
+    </li>
   `;
 }
 
 async function init() {
   const response = await fetch("/assets/json/data.json").then((x) => x.json());
-  contents.innerHTML = response
-    .map((x) => {
-      return `
-      <ul class="menuLinks">
-        <li class="menuLink"><span onclick="clickLink(this)">${x.label}</span>
-        ${x.children?.length > 0 ? x.children.map((y) => childElemets(y)).join("") : ""}
-        </li>
-      </ul>
-      <hr>
-    `;
-    })
-    .join("");
-}
-
-function clickLink(p) {
-  const treeElements = p.parentElement.parentElement.querySelectorAll(".menuLinks");
-  treeElements.forEach((treeElement) => {
-    if (treeElement != null) {
-      treeElement.querySelectorAll("*").forEach((element) => {
-        element.classList.toggle("hidden");
-      });
-    }
-  });
+  menuLinks.innerHTML = response.map((x) => createItems(x) + "<hr>").join("");
 }
 
 init();
